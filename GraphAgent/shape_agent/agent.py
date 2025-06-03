efecda851527c2f0f19181087de963aec97cbf84
Chris Mague <mague@google.com>
2025-06-03 11:34:15 -0700

    new agent

---
+++ GraphAgent/shape_agent/agent.py
@@ -0,0 +1,13 @@
+from google.adk.agents import Agent
+from google.adk.tools.toolbox_tool import ToolboxTool
+
+toolbox = ToolboxTool("http://127.0.0.1:5000")
+tools = toolbox.get_toolset(toolset_name='shapes')
+
+root_agent = Agent(
+    model='gemini-2.0-flash-001',
+    name='shape_agent',
+    description='Given an zip code, last 4, email address and a shipping address look for similar transaction shap',
+    instruction='Extract the email address, last4 credit card digits, zipcode, and shipping address search from the question and query the toolbox to return the timestamp, is_suspect and the similar shipping address as a table',
+    tools=tools,
+)
